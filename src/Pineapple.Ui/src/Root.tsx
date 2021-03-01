799040cc575edda15155d2ef6aa759fc8fb8f265
Grzegorz Wodniczak <kontakt@vebaspect.pl>
2021-03-01 18:19:59 +0100

    Integracja UI z biblioteką "Material-UI"

--- src/Pineapple.Ui/src/Root.tsx
+++ src/Pineapple.Ui/src/Root.tsx
@@ -1,10 +1,32 @@
 import React from 'react';
 
+import Container from '@material-ui/core/Container';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+
 const Root = () => {
+  const theme = createMuiTheme();
+
   return (
-    <div>
-      {'Hello world!'}
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Container>
+        <Grid container>
+          <Grid item xs={12}>
+            {'Hello world!'}
+          </Grid>
+        </Grid>
+        <Grid container>
+          <Grid item xs={3}>
+            {'Hello world!'}
+          </Grid>
+          <Grid item xs={9}>
+            {'Hello world!'}
+          </Grid>
+        </Grid>
+      </Container>
+    </ThemeProvider>
   )
 }
 
