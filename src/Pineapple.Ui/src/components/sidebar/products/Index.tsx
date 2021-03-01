6654f30864dd3628d4b5328fec97f2f7f43a9d6b
Grzegorz Wodniczak <kontakt@vebaspect.pl>
2021-03-01 22:07:42 +0100

    UI - pasek boczny - rozwój komponentu prezentującego listę produktów

--- src/Pineapple.Ui/src/components/sidebar/products/Index.tsx
+++ src/Pineapple.Ui/src/components/sidebar/products/Index.tsx
@@ -1,21 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import Box from '@material-ui/core/Box';
+import Collapse from '@material-ui/core/Collapse';
+import ExpandLess from '@material-ui/icons/ExpandLess';
+import ExpandMore from '@material-ui/icons/ExpandMore';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Tooltip from '@material-ui/core/Tooltip';
 
 interface Props {
-    items: {
-        name: string,
-        description: string,
-    }[];
+  // Produkty.
+  items: {
+    // Identyfikator.
+    id: string,
+    // Nazwa.
+    name: string,
+    // Opis.
+    description: string,
+  }[];
 };
 
-const Products = ({ items } : Props) => {
+const Products = ({ items }: Props) => {
+  // Flaga określająca, czy lista produktów jest rozwinięta.
+  const [isExpanded, setIsExpanded] = React.useState(true);
+
+  const onHeaderClick = () => {
+    setIsExpanded(!isExpanded);
+  };
+
   return (
-    <>
-      <Box>
-        Produkty ({items.length})
-      </Box>
-    </>
+    <List component="nav">
+      <ListItem button onClick={onHeaderClick}>
+        <ListItemText>
+          Produkty ({items.length})
+        </ListItemText>
+        {isExpanded ? <ExpandLess /> : <ExpandMore />}
+      </ListItem>
+      <Collapse in={isExpanded}>
+        <List component="div">
+          {
+            items.length > 0
+            ? items.map(item => {
+              return (
+                <Tooltip key={item.id} title={item.description}>
+                  <ListItem button>
+                    <ListItemText>
+                      {item.name}
+                    </ListItemText>
+                  </ListItem>
+                </Tooltip>
+              )
+            })
+            : null
+          }
+        </List>
+      </Collapse>
+    </List>
   )
 }
 
