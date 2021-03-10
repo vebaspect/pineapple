49724536a6b8d20629c7dda96d30a568a9868834
Grzegorz Wodniczak <kontakt@vebaspect.pl>
2021-03-10 20:46:54 +0100

    UI - prezentowanie logów dotyczących wdrożeń

--- src/Pineapple.Ui/src/components/content/implementations/Index.tsx
+++ src/Pineapple.Ui/src/components/content/implementations/Index.tsx
@@ -1,9 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import Box from '@material-ui/core/Box';
+
+import Logs from './logs';
 
 const Implementations = () => {
+  // Flaga określająca, czy lista logów została pobrana z API.
+  const [isLogsFetched, setIsLogsFetched] = useState(false);
+  // Lista logów.
+  const [logs, setLogs] = useState([]);
+
+  useEffect(() => {
+    fetchLogs();
+  }, []);
+
+  const fetchLogs = async () => {
+    await fetch(`${window['env'].API_URL}/logs/implementations`)
+      .then(response => response.json())
+      .then(data => {
+        setIsLogsFetched(true);
+        setLogs(data);
+      });
+  };
+
   return (
     <>
-      Wdrożenia
+      <Box
+        fontSize="h6.fontSize"
+        m={2}
+        textAlign="center"
+      >
+        Wdrożenia
+      </Box>
+      <Box>
+        <Box>
+          Ostatnie aktywności:
+        </Box>
+        <Logs
+          isDataFetched={isLogsFetched}
+          data={logs}
+        />
+      </Box>
     </>
   );
 }
