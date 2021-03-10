d33b0213c7d8cba28745339b15fb656091af4161
Grzegorz Wodniczak <kontakt@vebaspect.pl>
2021-03-10 20:40:12 +0100

    API - uporządkowanie kodu

--- src/Pineapple.Core/Handlers/GetLogsCommandHandler.cs
+++ src/Pineapple.Core/Handlers/GetLogsCommandHandler.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Pineapple.Core.Commands;
@@ -6,7 +7,6 @@ using Pineapple.Core.Dto;
 using Pineapple.Core.Storage.Database;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
-using System.Collections.Generic;
 
 namespace Pineapple.Core.Handler
 {
