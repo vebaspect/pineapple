b2a7fb7c867c803d38983c2285e677e76036679a
Grzegorz Wodniczak <kontakt@vebaspect.pl>
2021-03-10 21:03:18 +0100

    UI - prezentowanie logów dotyczących produktu

--- src/Pineapple.Ui/src/components/content/product/Index.tsx
+++ src/Pineapple.Ui/src/components/content/product/Index.tsx
@@ -1,12 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+
+import Box from '@material-ui/core/Box';
+
+import Logs from './logs';
 
 const Product = () => {
   const { productId } = useParams();
 
+  // Flaga określająca, czy lista logów została pobrana z API.
+  const [isLogsFetched, setIsLogsFetched] = useState(false);
+  // Lista logów.
+  const [logs, setLogs] = useState([]);
+
+  useEffect(() => {
+    fetchLogs();
+  }, [productId]);
+
+  const fetchLogs = async () => {
+    await fetch(`${window['env'].API_URL}/logs/products/${productId}`)
+      .then(response => response.json())
+      .then(data => {
+        setIsLogsFetched(true);
+        setLogs(data);
+      });
+  };
+
   return (
     <>
-      Produkt {productId}
+      <Box
+        fontSize="h6.fontSize"
+        m={2}
+        textAlign="center"
+      >
+        Produkt
+      </Box>
+      <Box>
+        <Box>
+          Ostatnie aktywności:
+        </Box>
+        <Logs
+          isDataFetched={isLogsFetched}
+          data={logs}
+        />
+      </Box>
     </>
   );
 }
