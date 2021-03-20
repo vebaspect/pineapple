c80046431b4ceb4af589b9ba4ab24e4e8cfcbc3c
Grzegorz Wodniczak <kontakt@vebaspect.pl>
2021-03-20 22:23:22 +0100

    API - budowanie oraz publikowanie aplikacji pod systemy operacyjne Windows oraz Linux

--- scripts/build-api.ps1
+++ scripts/build-api.ps1
@@ -7,4 +7,8 @@ If (Test-Path $scriptDir\dist\Pineapple.Api) {
         -Recurse
 }
 
-dotnet build $scriptDir\src\Pineapple.Api\Pineapple.Api.csproj -o $scriptDir\dist\Pineapple.Api -c Build
+# Runtime: win10-x64
+dotnet build $scriptDir\src\Pineapple.Api\Pineapple.Api.csproj -o $scriptDir\dist\Pineapple.Api\win10-x64 -r win10-x64 -c Build
+
+# Runtime: linux-x64
+dotnet build $scriptDir\src\Pineapple.Api\Pineapple.Api.csproj -o $scriptDir\dist\Pineapple.Api\linux-x64 -r linux-x64 -c Build
--- scripts/publish-api.ps1
+++ scripts/publish-api.ps1
@@ -7,7 +7,11 @@ If (Test-Path $scriptDir\dist\Pineapple.Api) {
         -Recurse
 }
 
-dotnet publish $scriptDir\src\Pineapple.Api\Pineapple.Api.csproj -o $scriptDir\dist\Pineapple.Api -c Publish --self-contained true -p:PublishSingleFile=true -p:PublishTrimmed=true -p:IncludeAllContentForSelfExtract=true
+# Runtime: win10-x64
+dotnet publish $scriptDir\src\Pineapple.Api\Pineapple.Api.csproj -o $scriptDir\dist\Pineapple.Api\win10-x64 -r win10-x64 -c Publish --self-contained true -p:PublishSingleFile=true -p:PublishTrimmed=true -p:IncludeAllContentForSelfExtract=true
+
+# Runtime: linux-x64
+dotnet publish $scriptDir\src\Pineapple.Api\Pineapple.Api.csproj -o $scriptDir\dist\Pineapple.Api\linux-x64 -r linux-x64 -c Publish --self-contained true -p:PublishSingleFile=true -p:PublishTrimmed=true -p:IncludeAllContentForSelfExtract=true
 
 # Skopiowanie pliku "CHANGELOG.md".
 $source = "$scriptDir\CHANGELOG.md"
