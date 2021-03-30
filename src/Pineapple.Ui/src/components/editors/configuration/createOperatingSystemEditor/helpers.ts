5487539c9a889c6deb5f9ba26f184d4e18ed5ee5
Grzegorz Wodniczak <kontakt@vebaspect.pl>
2021-03-30 23:05:52 +0200

    API+UI - dodawanie systemów operacyjnych (2)

---
+++ src/Pineapple.Ui/src/components/editors/configuration/createOperatingSystemEditor/helpers.ts
@@ -0,0 +1,34 @@
+import {
+  FormState,
+  FormStateValidationResult,
+} from './interfaces';
+
+export const initialFormState = () : FormState => {
+  const formState: FormState = {
+    name: '',
+    symbol: '',
+    description: '',
+  };
+  return formState;
+};
+
+export const validateFormState = (formState: FormState) : FormStateValidationResult => {
+  const formStateValidationResult: FormStateValidationResult = {
+    isValid: true,
+    details: {
+      name: null,
+      symbol: null,
+    },
+  };
+
+  if (!formState.name) {
+    formStateValidationResult.isValid = false;
+    formStateValidationResult.details.name = 'Pole wymagane.';
+  }
+  if (!formState.symbol) {
+    formStateValidationResult.isValid = false;
+    formStateValidationResult.details.symbol = 'Pole wymagane.';
+  }
+
+  return formStateValidationResult;
+};
---
+++ src/Pineapple.Ui/src/components/editors/configuration/createOperatingSystemEditor/interfaces.ts
@@ -0,0 +1,20 @@
+export interface FormState {
+  // Nazwa.
+  name: string,
+  // Symbol.
+  symbol: string,
+  // Opis.
+  description: string,
+}
+
+export interface FormStateValidationResult {
+  // Flaga określająca, czy stan formularza jest poprawny.
+  isValid: boolean,
+  // Stan poszczególnych pól formularza.
+  details: {
+    // Nazwa.
+    name: string,
+    // Symbol.
+    symbol: string,
+  },
+}
