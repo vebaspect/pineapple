10c46712d3c8dedbc1dbeef2dc73f6e641ae2cd5
Grzegorz Wodniczak <kontakt@vebaspect.pl>
2021-05-02 11:34:59 +0200

    UI - poprawa jakości kodu

--- src/Pineapple.Ui/src/components/content/implementations/Index.tsx
+++ src/Pineapple.Ui/src/components/content/implementations/Index.tsx
@@ -14,6 +14,7 @@ import Paper from '@material-ui/core/Paper';
 import AddIcon from '@material-ui/icons/Add';
 
 import Logs from '../../logs';
+
 import List from './List';
 
 const useStyles = makeStyles(() =>
