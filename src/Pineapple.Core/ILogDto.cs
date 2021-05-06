76fb025a1a08c483f2adc1b156ac347ad6399654
Grzegorz Wodniczak <kontakt@vebaspect.pl>
2021-05-06 13:18:01 +0200

    API - uzupełnienie brakujących komentarzy

--- src/Pineapple.Core/ILogDto.cs
+++ src/Pineapple.Core/ILogDto.cs
@@ -6,26 +6,59 @@ namespace Pineapple.Core
 {
     public interface ILogDto : IDto
     {
+        /// <summary>
+        /// Identyfikator.
+        /// </summary>
         Guid Id { get; }
 
+        /// <summary>
+        /// Data utworzenia.
+        /// </summary>
         DateTime CreationDate { get; }
 
+        /// <summary>
+        /// Data modyfikacji.
+        /// </summary>
         DateTime ModificationDate { get; }
 
+        /// <summary>
+        /// Flaga określająca, czy log został usunięty.
+        /// </summary>
         bool IsDeleted { get; }
 
+        /// <summary>
+        /// Typ.
+        /// </summary>
         string Type { get; }
 
+        /// <summary>
+        /// Kategoria.
+        /// </summary>
         string Category { get; }
 
+        /// <summary>
+        /// Identyfikator właściciela.
+        /// </summary>
         Guid OwnerId { get; }
 
+        /// <summary>
+        /// Imię i nazwisko właściciela.
+        /// </summary>
         string OwnerFullName { get; }
 
+        /// <summary>
+        /// Encja.
+        /// </summary>
         EntityDto Entity { get; }
 
+        /// <summary>
+        /// Encje nadrzędne.
+        /// </summary>
         List<EntityDto> ParentEntities { get; }
 
+        /// <summary>
+        /// Opis.
+        /// </summary>
         string Description { get; }
     }
 }
