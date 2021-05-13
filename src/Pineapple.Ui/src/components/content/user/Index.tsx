bf2ec999de292f9a24a12372edb9bfd18f0b74c3
Grzegorz Wodniczak <kontakt@vebaspect.pl>
2021-05-13 21:44:42 +0200

    UI - widok szczegółów użytkownika - loadery w nawigacji + obsługa błędów podczas pobierania danych z API

--- src/Pineapple.Ui/src/components/content/user/Index.tsx
+++ src/Pineapple.Ui/src/components/content/user/Index.tsx
@@ -1,18 +1,39 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import { Link as RouterLink, useParams } from 'react-router-dom';
 
+import {
+  createStyles,
+  makeStyles,
+} from '@material-ui/core/styles';
+
 import Box from '@material-ui/core/Box';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import Link from '@material-ui/core/Link';
 import Paper from '@material-ui/core/Paper';
+
+import SentimentVeryDissatisfiedIcon from '@material-ui/icons/SentimentVeryDissatisfied';
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    userFetchingFailureIcon: {
+      color: '#f50057',
+      fontSize: 12,
+    },
+  }),
+);
 
 import Details from './Details';
 
 const User: React.VFC = () => {
+  const styles = useStyles();
+
   // Identyfikator użytkownika.
   const { userId } = useParams();
 
   // Flaga określająca, czy użytkownik został pobrany z API.
   const [isUserFetched, setIsUserFetched] = useState(false);
+  // Flaga określająca, czy pobieranie użytkownika z API zakończyło się błędem.
+  const [isUserFetchingFailure, setIsUserFetchingFailure] = useState(false);
   // Użytkownik.
   const [user, setUser] = useState(null);
 
@@ -22,6 +43,10 @@ const User: React.VFC = () => {
       .then((data) => {
         setIsUserFetched(true);
         setUser(data);
+      })
+      .catch(() => {
+        setIsUserFetched(true);
+        setIsUserFetchingFailure(true);
       });
   }, [userId]);
 
@@ -46,34 +71,63 @@ const User: React.VFC = () => {
           isUserFetched
             ? (
               <Box component="span">
-                {user?.fullName}
-              </Box>)
-            : ''
+                {
+                  isUserFetchingFailure
+                    ? <SentimentVeryDissatisfiedIcon className={styles.userFetchingFailureIcon} />
+                    : user?.fullName
+                }
+              </Box>
+            )
+            : (
+              <Box component="span">
+                <CircularProgress size={10} />
+              </Box>
+            )
         }
       </Box>
       <Box
         mb={3}
       >
-        <Paper>
-          <Box
-            border={1}
-            borderLeft={0}
-            borderRight={0}
-            borderTop={0}
-            borderColor="#e0e0e0"
-            py={1.5}
-            textAlign="center"
-          >
-            Szczegóły
-          </Box>
-          <Details
-            isDataFetched={isUserFetched}
-            fullName={user?.fullName}
-            login={user?.login}
-            phone={user?.phone}
-            email={user?.email}
-          />
-        </Paper>
+        {
+          isUserFetchingFailure
+            ? (
+              <Paper>
+                <Box
+                  border={1}
+                  borderLeft={0}
+                  borderRight={0}
+                  borderTop={0}
+                  borderColor="#e0e0e0"
+                  py={1.5}
+                  textAlign="center"
+                >
+                  Pobieranie informacji nt. użytkownika zakończyło się błędem.
+                </Box>
+              </Paper>
+            )
+            : (
+              <Paper>
+                <Box
+                  border={1}
+                  borderLeft={0}
+                  borderRight={0}
+                  borderTop={0}
+                  borderColor="#e0e0e0"
+                  py={1.5}
+                  textAlign="center"
+                >
+                  Szczegóły
+                </Box>
+                <Details
+                  isDataFetched={isUserFetched}
+                  fullName={user?.fullName}
+                  login={user?.login}
+                  phone={user?.phone}
+                  email={user?.email}
+                />
+              </Paper>
+            )
+        }
       </Box>
     </>
   );
