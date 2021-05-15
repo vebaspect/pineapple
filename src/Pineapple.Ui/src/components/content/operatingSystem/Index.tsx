2cb46826c7f0a73ecac3800d613d20fc18f644bb
Grzegorz Wodniczak <kontakt@vebaspect.pl>
2021-05-15 18:56:24 +0200

    UI - widok szczegółów systemu operacyjnego - loadery w nawigacji + obsługa błędów podczas pobierania danych z API

--- src/Pineapple.Ui/src/components/content/operatingSystem/Index.tsx
+++ src/Pineapple.Ui/src/components/content/operatingSystem/Index.tsx
@@ -1,18 +1,39 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import { Link as RouterLink, useParams } from 'react-router-dom';
 
+import {
+  createStyles,
+  makeStyles,
+} from '@material-ui/core/styles';
+
 import Box from '@material-ui/core/Box';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import Link from '@material-ui/core/Link';
 import Paper from '@material-ui/core/Paper';
+
+import SentimentVeryDissatisfiedIcon from '@material-ui/icons/SentimentVeryDissatisfied';
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    operatingSystemFetchingFailureIcon: {
+      color: '#f50057',
+      fontSize: 12,
+    },
+  }),
+);
 
 import Details from './Details';
 
 const OperatingSystem: React.VFC = () => {
+  const styles = useStyles();
+
   // Identyfikator systemu operacyjnego.
   const { operatingSystemId } = useParams();
 
   // Flaga określająca, czy system operacyjny został pobrany z API.
   const [isOperatingSystemFetched, setIsOperatingSystemFetched] = useState(false);
+  // Flaga określająca, czy pobieranie systemu operacyjnego z API zakończyło się błędem.
+  const [isOperatingSystemFetchingFailure, setIsOperatingSystemFetchingFailure] = useState(false);
   // System operacyjny.
   const [operatingSystem, setOperatingSystem] = useState(null);
 
@@ -22,6 +43,10 @@ const OperatingSystem: React.VFC = () => {
       .then((data) => {
         setIsOperatingSystemFetched(true);
         setOperatingSystem(data);
+      })
+      .catch(() => {
+        setIsOperatingSystemFetched(true);
+        setIsOperatingSystemFetchingFailure(true);
       });
   }, [operatingSystemId]);
 
@@ -53,33 +78,62 @@ const OperatingSystem: React.VFC = () => {
           isOperatingSystemFetched
             ? (
               <Box component="span">
-                {operatingSystem?.name}
-              </Box>)
-            : ''
+                {
+                  isOperatingSystemFetchingFailure
+                    ? <SentimentVeryDissatisfiedIcon className={styles.operatingSystemFetchingFailureIcon} />
+                    : operatingSystem?.name
+                }
+              </Box>
+            )
+            : (
+              <Box component="span">
+                <CircularProgress size={10} />
+              </Box>
+            )
         }
       </Box>
       <Box
         mb={3}
       >
-        <Paper>
-          <Box
-            border={1}
-            borderLeft={0}
-            borderRight={0}
-            borderTop={0}
-            borderColor="#e0e0e0"
-            py={1.5}
-            textAlign="center"
-          >
-            Szczegóły
-          </Box>
-          <Details
-            isDataFetched={isOperatingSystemFetched}
-            name={operatingSystem?.name}
-            symbol={operatingSystem?.symbol}
-            description={operatingSystem?.description}
-          />
-        </Paper>
+        {
+          isOperatingSystemFetchingFailure
+            ? (
+              <Paper>
+                <Box
+                  border={1}
+                  borderLeft={0}
+                  borderRight={0}
+                  borderTop={0}
+                  borderColor="#e0e0e0"
+                  py={1.5}
+                  textAlign="center"
+                >
+                  Pobieranie informacji nt. systemu operacyjnego zakończyło się błędem.
+                </Box>
+              </Paper>
+            )
+            : (
+              <Paper>
+                <Box
+                  border={1}
+                  borderLeft={0}
+                  borderRight={0}
+                  borderTop={0}
+                  borderColor="#e0e0e0"
+                  py={1.5}
+                  textAlign="center"
+                >
+                  Szczegóły
+                </Box>
+                <Details
+                  isDataFetched={isOperatingSystemFetched}
+                  name={operatingSystem?.name}
+                  symbol={operatingSystem?.symbol}
+                  description={operatingSystem?.description}
+                />
+              </Paper>
+            )
+        }
       </Box>
     </>
   );
