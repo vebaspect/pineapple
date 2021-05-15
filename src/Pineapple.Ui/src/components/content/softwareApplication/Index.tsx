4928096f6fc4545faefaa0c5d98ea8a4f0209553
Grzegorz Wodniczak <kontakt@vebaspect.pl>
2021-05-15 18:47:33 +0200

    UI - widok szczegółów oprogramowania - loadery w nawigacji + obsługa błędów podczas pobierania danych z API

--- src/Pineapple.Ui/src/components/content/softwareApplication/Index.tsx
+++ src/Pineapple.Ui/src/components/content/softwareApplication/Index.tsx
@@ -1,18 +1,39 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import { Link as RouterLink, useParams } from 'react-router-dom';
 
+import {
+  createStyles,
+  makeStyles,
+} from '@material-ui/core/styles';
+
 import Box from '@material-ui/core/Box';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import Link from '@material-ui/core/Link';
 import Paper from '@material-ui/core/Paper';
+
+import SentimentVeryDissatisfiedIcon from '@material-ui/icons/SentimentVeryDissatisfied';
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    softwareApplicationFetchingFailureIcon: {
+      color: '#f50057',
+      fontSize: 12,
+    },
+  }),
+);
 
 import Details from './Details';
 
 const SoftwareApplication: React.VFC = () => {
+  const styles = useStyles();
+
   // Identyfikator oprogramowania.
   const { softwareApplicationId } = useParams();
 
   // Flaga określająca, czy oprogramowanie zostało pobrane z API.
   const [isSoftwareApplicationFetched, setIsSoftwareApplicationFetched] = useState(false);
+  // Flaga określająca, czy pobieranie oprogramowania z API zakończyło się błędem.
+  const [isSoftwareApplicationFetchingFailure, setIsSoftwareApplicationFetchingFailure] = useState(false);
   // Oprogramowanie.
   const [softwareApplication, setSoftwareApplication] = useState(null);
 
@@ -22,6 +43,10 @@ const SoftwareApplication: React.VFC = () => {
       .then((data) => {
         setIsSoftwareApplicationFetched(true);
         setSoftwareApplication(data);
+      })
+      .catch(() => {
+        setIsSoftwareApplicationFetched(true);
+        setIsSoftwareApplicationFetchingFailure(true);
       });
   }, [softwareApplicationId]);
 
@@ -53,33 +78,62 @@ const SoftwareApplication: React.VFC = () => {
           isSoftwareApplicationFetched
             ? (
               <Box component="span">
-                {softwareApplication?.name}
-              </Box>)
-            : ''
+                {
+                  isSoftwareApplicationFetchingFailure
+                    ? <SentimentVeryDissatisfiedIcon className={styles.softwareApplicationFetchingFailureIcon} />
+                    : softwareApplication?.name
+                }
+              </Box>
+            )
+            : (
+              <Box component="span">
+                <CircularProgress size={10} />
+              </Box>
+            )
         }
       </Box>
       <Box
         mb={3}
       >
-        <Paper>
-          <Box
-            border={1}
-            borderLeft={0}
-            borderRight={0}
-            borderTop={0}
-            borderColor="#e0e0e0"
-            py={1.5}
-            textAlign="center"
-          >
-            Szczegóły
-          </Box>
-          <Details
-            isDataFetched={isSoftwareApplicationFetched}
-            name={softwareApplication?.name}
-            symbol={softwareApplication?.symbol}
-            description={softwareApplication?.description}
-          />
-        </Paper>
+        {
+          isSoftwareApplicationFetchingFailure
+            ? (
+              <Paper>
+                <Box
+                  border={1}
+                  borderLeft={0}
+                  borderRight={0}
+                  borderTop={0}
+                  borderColor="#e0e0e0"
+                  py={1.5}
+                  textAlign="center"
+                >
+                  Pobieranie informacji nt. oprogramowania zakończyło się błędem.
+                </Box>
+              </Paper>
+            )
+            : (
+              <Paper>
+                <Box
+                  border={1}
+                  borderLeft={0}
+                  borderRight={0}
+                  borderTop={0}
+                  borderColor="#e0e0e0"
+                  py={1.5}
+                  textAlign="center"
+                >
+                  Szczegóły
+                </Box>
+                <Details
+                  isDataFetched={isSoftwareApplicationFetched}
+                  name={softwareApplication?.name}
+                  symbol={softwareApplication?.symbol}
+                  description={softwareApplication?.description}
+                />
+              </Paper>
+            )
+        }
       </Box>
     </>
   );
