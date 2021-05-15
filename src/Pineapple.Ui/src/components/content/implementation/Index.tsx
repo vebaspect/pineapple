e8ae004a97c99d6390bab1a5a9fe73d7d633e23e
Grzegorz Wodniczak <kontakt@vebaspect.pl>
2021-05-15 19:24:57 +0200

    UI - widok szczegółów wdrożenia - loadery w nawigacji + obsługa błędów podczas pobierania danych z API

--- src/Pineapple.Ui/src/components/content/implementation/Index.tsx
+++ src/Pineapple.Ui/src/components/content/implementation/Index.tsx
@@ -8,10 +8,12 @@ import {
 
 import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import Link from '@material-ui/core/Link';
 import Paper from '@material-ui/core/Paper';
 
 import AddIcon from '@material-ui/icons/Add';
+import SentimentVeryDissatisfiedIcon from '@material-ui/icons/SentimentVeryDissatisfied';
 
 import DialogWindow from '../../windows/DialogWindow';
 import ErrorWindow from '../../windows/ErrorWindow';
@@ -26,6 +28,10 @@ const useStyles = makeStyles(() =>
       backgroundColor: '#4caf50',
       color: '#fff',
     },
+    implementationFetchingFailureIcon: {
+      color: '#f50057',
+      fontSize: 12,
+    },
   }),
 );
 
@@ -38,11 +44,15 @@ const Implementation: React.VFC = () => {
 
   // Flaga określająca, czy wdrożenie zostało pobrane z API.
   const [isImplementationFetched, setIsImplementationFetched] = useState(false);
+  // Flaga określająca, czy pobieranie wdrożenia z API zakończyło się błędem.
+  const [isImplementationFetchingFailure, setIsImplementationFetchingFailure] = useState(false);
   // Wdrożenie.
   const [implementation, setImplementation] = useState(null);
 
   // Flaga określająca, czy lista środowisk została pobrana z API.
   const [isEnvironmentsFetched, setIsEnvironmentsFetched] = useState(false);
+  // Flaga określająca, czy pobieranie listy środowisk z API zakończyło się błędem.
+  const [isEnvironmentsFetchingFailure, setIsEnvironmentsFetchingFailure] = useState(false);
   // Lista środowisk.
   const [environments, setEnvironments] = useState([]);
 
@@ -86,6 +96,10 @@ const Implementation: React.VFC = () => {
       .then((data) => {
         setIsImplementationFetched(true);
         setImplementation(data);
+      })
+      .catch(() => {
+        setIsImplementationFetched(true);
+        setIsImplementationFetchingFailure(true);
       });
   }, [implementationId]);
 
@@ -99,6 +113,10 @@ const Implementation: React.VFC = () => {
       .then((data) => {
         setIsEnvironmentsFetched(true);
         setEnvironments(data);
+      })
+      .catch(() => {
+        setIsEnvironmentsFetched(true);
+        setIsEnvironmentsFetchingFailure(true);
       });
   }, [implementationId]);
 
@@ -168,110 +186,179 @@ const Implementation: React.VFC = () => {
           isImplementationFetched
             ? (
               <Box component="span">
-                {implementation?.name}
-              </Box>)
-            : ''
+                {
+                  isImplementationFetchingFailure
+                    ? <SentimentVeryDissatisfiedIcon className={styles.implementationFetchingFailureIcon} />
+                    : implementation?.name
+                }
+              </Box>
+            )
+            : (
+              <Box component="span">
+                <CircularProgress size={10} />
+              </Box>
+            )
         }
       </Box>
       <Box
         mb={3}
       >
-        <Paper>
-          <Box
-            border={1}
-            borderLeft={0}
-            borderRight={0}
-            borderTop={0}
-            borderColor="#e0e0e0"
-            py={1.5}
-            textAlign="center"
-          >
-            Szczegóły
-          </Box>
-          <Details
-            isDataFetched={isImplementationFetched}
-            name={implementation?.name}
-            managerId={implementation?.managerId}
-            managerFullName={implementation?.managerFullName}
-            description={implementation?.description}
-          />
-        </Paper>
+        {
+          isImplementationFetchingFailure
+            ? (
+              <Paper>
+                <Box
+                  border={1}
+                  borderLeft={0}
+                  borderRight={0}
+                  borderTop={0}
+                  borderColor="#e0e0e0"
+                  py={1.5}
+                  textAlign="center"
+                >
+                  Pobieranie informacji nt. wdrożenia zakończyło się błędem.
+                </Box>
+              </Paper>
+            )
+            : (
+              <Paper>
+                <Box
+                  border={1}
+                  borderLeft={0}
+                  borderRight={0}
+                  borderTop={0}
+                  borderColor="#e0e0e0"
+                  py={1.5}
+                  textAlign="center"
+                >
+                  Szczegóły
+                </Box>
+                <Details
+                  isDataFetched={isImplementationFetched}
+                  name={implementation?.name}
+                  managerId={implementation?.managerId}
+                  managerFullName={implementation?.managerFullName}
+                  description={implementation?.description}
+                />
+              </Paper>
+            )
+        }
       </Box>
       <Box
         mb={3}
       >
-        <Paper>
-          <Box
-            border={1}
-            borderLeft={0}
-            borderRight={0}
-            borderTop={0}
-            borderColor="#e0e0e0"
-            py={1.5}
-            textAlign="center"
-          >
-            Środowiska
-          </Box>
-          <List
-            isDataFetched={isEnvironmentsFetched}
-            data={environments}
-            implementationId={implementationId}
-            onEdit={editEnvironment}
-            onDelete={deleteEnvironment}
-          />
-          <Box
-            p={1.5}
-            textAlign="right"
-          >
-            <Button
-              className={styles.add}
-              size="small"
-              startIcon={<AddIcon />}
-              variant="contained"
-              onClick={addEnvironment}
-            >
-              Dodaj
-            </Button>
-          </Box>
-        </Paper>
+        {
+          isEnvironmentsFetchingFailure
+            ? (
+              <Paper>
+                <Box
+                  border={1}
+                  borderLeft={0}
+                  borderRight={0}
+                  borderTop={0}
+                  borderColor="#e0e0e0"
+                  py={1.5}
+                  textAlign="center"
+                >
+                  Pobieranie informacji nt. środowisk zakończyło się błędem.
+                </Box>
+              </Paper>
+            )
+            : (
+              <Paper>
+                <Box
+                  border={1}
+                  borderLeft={0}
+                  borderRight={0}
+                  borderTop={0}
+                  borderColor="#e0e0e0"
+                  py={1.5}
+                  textAlign="center"
+                >
+                  Środowiska
+                </Box>
+                <List
+                  isDataFetched={isEnvironmentsFetched}
+                  data={environments}
+                  implementationId={implementationId}
+                  onEdit={editEnvironment}
+                  onDelete={deleteEnvironment}
+                />
+                <Box
+                  p={1.5}
+                  textAlign="right"
+                >
+                  <Button
+                    className={styles.add}
+                    size="small"
+                    startIcon={<AddIcon />}
+                    variant="contained"
+                    onClick={addEnvironment}
+                  >
+                    Dodaj
+                  </Button>
+                </Box>
+              </Paper>
+            )
+        }
       </Box>
       <Box
         mb={3}
       >
-        <Paper>
-          <Box
-            border={1}
-            borderLeft={0}
-            borderRight={0}
-            borderTop={0}
-            borderColor="#e0e0e0"
-            py={1.5}
-            textAlign="center"
-          >
-            Ostatnie aktywności
-          </Box>
-          <Box
-            mx={2}
-          >
-            <Logs
-              isDataFetched={isLogsFetched}
-              data={logs}
-            />
-          </Box>
-          <Box
-            border={1}
-            borderBottom={0}
-            borderLeft={0}
-            borderRight={0}
-            borderColor="#e0e0e0"
-            py={1.5}
-            textAlign="center"
-          >
-            <Link onClick={fetchMoreLogs}>
-              Pobierz więcej
-            </Link>
-          </Box>
-        </Paper>
+        {
+          isImplementationFetchingFailure
+            ? (
+              <Paper>
+                <Box
+                  border={1}
+                  borderLeft={0}
+                  borderRight={0}
+                  borderTop={0}
+                  borderColor="#e0e0e0"
+                  py={1.5}
+                  textAlign="center"
+                >
+                  Pobieranie informacji nt. logów zakończyło się błędem.
+                </Box>
+              </Paper>
+            )
+            : (
+              <Paper>
+                <Box
+                  border={1}
+                  borderLeft={0}
+                  borderRight={0}
+                  borderTop={0}
+                  borderColor="#e0e0e0"
+                  py={1.5}
+                  textAlign="center"
+                >
+                  Ostatnie aktywności
+                </Box>
+                <Box
+                  mx={2}
+                >
+                  <Logs
+                    isDataFetched={isLogsFetched}
+                    data={logs}
+                  />
+                </Box>
+                <Box
+                  border={1}
+                  borderBottom={0}
+                  borderLeft={0}
+                  borderRight={0}
+                  borderColor="#e0e0e0"
+                  py={1.5}
+                  textAlign="center"
+                >
+                  <Link onClick={fetchMoreLogs}>
+                    Pobierz więcej
+                  </Link>
+                </Box>
+              </Paper>
+            )
+        }
       </Box>
       <DialogWindow
         isOpen={isDeleteEnvironmentDialogWindowOpen}
