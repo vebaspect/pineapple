b6709b1337b1d141bfd7ebc400186e449dba4cce
Grzegorz Wodniczak <kontakt@vebaspect.pl>
2021-05-15 18:52:24 +0200

    UI - widok szczegółów typu komponentu - loadery w nawigacji + obsługa błędów podczas pobierania danych z API

--- src/Pineapple.Ui/src/components/content/componentType/Index.tsx
+++ src/Pineapple.Ui/src/components/content/componentType/Index.tsx
@@ -1,18 +1,39 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import { Link as RouterLink, useParams } from 'react-router-dom';
 
+import {
+  createStyles,
+  makeStyles,
+} from '@material-ui/core/styles';
+
 import Box from '@material-ui/core/Box';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import Link from '@material-ui/core/Link';
 import Paper from '@material-ui/core/Paper';
+
+import SentimentVeryDissatisfiedIcon from '@material-ui/icons/SentimentVeryDissatisfied';
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    componentTypeFetchingFailureIcon: {
+      color: '#f50057',
+      fontSize: 12,
+    },
+  }),
+);
 
 import Details from './Details';
 
 const ComponentType: React.VFC = () => {
+  const styles = useStyles();
+
   // Identyfikator typu komponentu.
   const { componentTypeId } = useParams();
 
   // Flaga określająca, czy typ komponentu został pobrany z API.
   const [isComponentTypeFetched, setIsComponentTypeFetched] = useState(false);
+  // Flaga określająca, czy pobieranie typu komponentu z API zakończyło się błędem.
+  const [isComponentTypeFetchingFailure, setIsComponentTypeFetchingFailure] = useState(false);
   // Typ komponentu.
   const [componentType, setComponentType] = useState(null);
 
@@ -22,6 +43,10 @@ const ComponentType: React.VFC = () => {
       .then((data) => {
         setIsComponentTypeFetched(true);
         setComponentType(data);
+      })
+      .catch(() => {
+        setIsComponentTypeFetched(true);
+        setIsComponentTypeFetchingFailure(true);
       });
   }, [componentTypeId]);
 
@@ -53,33 +78,62 @@ const ComponentType: React.VFC = () => {
           isComponentTypeFetched
             ? (
               <Box component="span">
-                {componentType?.name}
-              </Box>)
-            : ''
+                {
+                  isComponentTypeFetchingFailure
+                    ? <SentimentVeryDissatisfiedIcon className={styles.componentTypeFetchingFailureIcon} />
+                    : componentType?.name
+                }
+              </Box>
+            )
+            : (
+              <Box component="span">
+                <CircularProgress size={10} />
+              </Box>
+            )
         }
       </Box>
       <Box
         mb={3}
       >
-        <Paper>
-          <Box
-            border={1}
-            borderLeft={0}
-            borderRight={0}
-            borderTop={0}
-            borderColor="#e0e0e0"
-            py={1.5}
-            textAlign="center"
-          >
-            Szczegóły
-          </Box>
-          <Details
-            isDataFetched={isComponentTypeFetched}
-            name={componentType?.name}
-            symbol={componentType?.symbol}
-            description={componentType?.description}
-          />
-        </Paper>
+        {
+          isComponentTypeFetchingFailure
+            ? (
+              <Paper>
+                <Box
+                  border={1}
+                  borderLeft={0}
+                  borderRight={0}
+                  borderTop={0}
+                  borderColor="#e0e0e0"
+                  py={1.5}
+                  textAlign="center"
+                >
+                  Pobieranie informacji nt. typu komponentu zakończyło się błędem.
+                </Box>
+              </Paper>
+            )
+            : (
+              <Paper>
+                <Box
+                  border={1}
+                  borderLeft={0}
+                  borderRight={0}
+                  borderTop={0}
+                  borderColor="#e0e0e0"
+                  py={1.5}
+                  textAlign="center"
+                >
+                  Szczegóły
+                </Box>
+                <Details
+                  isDataFetched={isComponentTypeFetched}
+                  name={componentType?.name}
+                  symbol={componentType?.symbol}
+                  description={componentType?.description}
+                />
+              </Paper>
+            )
+        }
       </Box>
     </>
   );
