ab75b72ce602e711b21bca0981c1fc3a5ab01cd8
Grzegorz Wodniczak <kontakt@vebaspect.pl>
2021-05-15 19:31:32 +0200

    UI - widok szczegółów produktu - loadery w nawigacji + obsługa błędów podczas pobierania danych z API

--- src/Pineapple.Ui/src/components/content/product/Index.tsx
+++ src/Pineapple.Ui/src/components/content/product/Index.tsx
@@ -8,10 +8,12 @@ import {
 
 import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import Link from '@material-ui/core/Link';
 import Paper from '@material-ui/core/Paper';
 
 import AddIcon from '@material-ui/icons/Add';
+import SentimentVeryDissatisfiedIcon from '@material-ui/icons/SentimentVeryDissatisfied';
 
 import DialogWindow from '../../windows/DialogWindow';
 import ErrorWindow from '../../windows/ErrorWindow';
@@ -26,6 +28,10 @@ const useStyles = makeStyles(() =>
       backgroundColor: '#4caf50',
       color: '#fff',
     },
+    productFetchingFailureIcon: {
+      color: '#f50057',
+      fontSize: 12,
+    },
   }),
 );
 
@@ -38,11 +44,15 @@ const Product: React.VFC = () => {
 
   // Flaga określająca, czy produkt został pobrany z API.
   const [isProductFetched, setIsProductFetched] = useState(false);
+  // Flaga określająca, czy pobieranie produktu z API zakończyło się błędem.
+  const [isProductFetchingFailure, setIsProductFetchingFailure] = useState(false);
   // Produkt.
   const [product, setProduct] = useState(null);
 
   // Flaga określająca, czy lista komponentów została pobrana z API.
   const [isComponentsFetched, setIsComponentsFetched] = useState(false);
+  // Flaga określająca, czy pobieranie listy komponentów z API zakończyło się błędem.
+  const [isComponentsFetchingFailure, setIsComponentsFetchingFailure] = useState(false);
   // Lista komponentów.
   const [components, setComponents] = useState([]);
 
@@ -86,6 +96,10 @@ const Product: React.VFC = () => {
       .then((data) => {
         setIsProductFetched(true);
         setProduct(data);
+      })
+      .catch(() => {
+        setIsProductFetched(true);
+        setIsProductFetchingFailure(true);
       });
   }, [productId]);
 
@@ -99,6 +113,10 @@ const Product: React.VFC = () => {
       .then((data) => {
         setIsComponentsFetched(true);
         setComponents(data);
+      })
+      .catch(() => {
+        setIsComponentsFetched(true);
+        setIsComponentsFetchingFailure(true);
       });
   }, [productId]);
 
@@ -168,108 +186,177 @@ const Product: React.VFC = () => {
           isProductFetched
             ? (
               <Box component="span">
-                {product?.name}
-              </Box>)
-            : ''
+                {
+                  isProductFetchingFailure
+                    ? <SentimentVeryDissatisfiedIcon className={styles.productFetchingFailureIcon} />
+                    : product?.name
+                }
+              </Box>
+            )
+            : (
+              <Box component="span">
+                <CircularProgress size={10} />
+              </Box>
+            )
         }
       </Box>
       <Box
         mb={3}
       >
-        <Paper>
-          <Box
-            border={1}
-            borderLeft={0}
-            borderRight={0}
-            borderTop={0}
-            borderColor="#e0e0e0"
-            py={1.5}
-            textAlign="center"
-          >
-            Szczegóły
-          </Box>
-          <Details
-            isDataFetched={isProductFetched}
-            name={product?.name}
-            description={product?.description}
-          />
-        </Paper>
+        {
+          isProductFetchingFailure
+            ? (
+              <Paper>
+                <Box
+                  border={1}
+                  borderLeft={0}
+                  borderRight={0}
+                  borderTop={0}
+                  borderColor="#e0e0e0"
+                  py={1.5}
+                  textAlign="center"
+                >
+                  Pobieranie informacji nt. produktu zakończyło się błędem.
+                </Box>
+              </Paper>
+            )
+            : (
+              <Paper>
+                <Box
+                  border={1}
+                  borderLeft={0}
+                  borderRight={0}
+                  borderTop={0}
+                  borderColor="#e0e0e0"
+                  py={1.5}
+                  textAlign="center"
+                >
+                  Szczegóły
+                </Box>
+                <Details
+                  isDataFetched={isProductFetched}
+                  name={product?.name}
+                  description={product?.description}
+                />
+              </Paper>
+            )
+        }
       </Box>
       <Box
         mb={3}
       >
-        <Paper>
-          <Box
-            border={1}
-            borderLeft={0}
-            borderRight={0}
-            borderTop={0}
-            borderColor="#e0e0e0"
-            py={1.5}
-            textAlign="center"
-          >
-            Komponenty
-          </Box>
-          <List
-            isDataFetched={isComponentsFetched}
-            data={components}
-            productId={productId}
-            onEdit={editComponent}
-            onDelete={deleteComponent}
-          />
-          <Box
-            p={1.5}
-            textAlign="right"
-          >
-            <Button
-              className={styles.add}
-              size="small"
-              startIcon={<AddIcon />}
-              variant="contained"
-              onClick={addComponent}
-            >
-              Dodaj
-            </Button>
-          </Box>
-        </Paper>
+        {
+          isComponentsFetchingFailure
+            ? (
+              <Paper>
+                <Box
+                  border={1}
+                  borderLeft={0}
+                  borderRight={0}
+                  borderTop={0}
+                  borderColor="#e0e0e0"
+                  py={1.5}
+                  textAlign="center"
+                >
+                  Pobieranie informacji nt. komponentów zakończyło się błędem.
+                </Box>
+              </Paper>
+            )
+            : (
+              <Paper>
+                <Box
+                  border={1}
+                  borderLeft={0}
+                  borderRight={0}
+                  borderTop={0}
+                  borderColor="#e0e0e0"
+                  py={1.5}
+                  textAlign="center"
+                >
+                  Komponenty
+                </Box>
+                <List
+                  isDataFetched={isComponentsFetched}
+                  data={components}
+                  productId={productId}
+                  onEdit={editComponent}
+                  onDelete={deleteComponent}
+                />
+                <Box
+                  p={1.5}
+                  textAlign="right"
+                >
+                  <Button
+                    className={styles.add}
+                    size="small"
+                    startIcon={<AddIcon />}
+                    variant="contained"
+                    onClick={addComponent}
+                  >
+                    Dodaj
+                  </Button>
+                </Box>
+              </Paper>
+            )
+        }
       </Box>
       <Box
         mb={3}
       >
-        <Paper>
-          <Box
-            border={1}
-            borderLeft={0}
-            borderRight={0}
-            borderTop={0}
-            borderColor="#e0e0e0"
-            py={1.5}
-            textAlign="center"
-          >
-            Ostatnie aktywności
-          </Box>
-          <Box
-            mx={2}
-          >
-            <Logs
-              isDataFetched={isLogsFetched}
-              data={logs}
-            />
-          </Box>
-          <Box
-            border={1}
-            borderBottom={0}
-            borderLeft={0}
-            borderRight={0}
-            borderColor="#e0e0e0"
-            py={1.5}
-            textAlign="center"
-          >
-            <Link onClick={fetchMoreLogs}>
-              Pobierz więcej
-            </Link>
-          </Box>
-        </Paper>
+        {
+          isProductFetchingFailure
+            ? (
+              <Paper>
+                <Box
+                  border={1}
+                  borderLeft={0}
+                  borderRight={0}
+                  borderTop={0}
+                  borderColor="#e0e0e0"
+                  py={1.5}
+                  textAlign="center"
+                >
+                  Pobieranie informacji nt. logów zakończyło się błędem.
+                </Box>
+              </Paper>
+            )
+            : (
+              <Paper>
+                <Box
+                  border={1}
+                  borderLeft={0}
+                  borderRight={0}
+                  borderTop={0}
+                  borderColor="#e0e0e0"
+                  py={1.5}
+                  textAlign="center"
+                >
+                  Ostatnie aktywności
+                </Box>
+                <Box
+                  mx={2}
+                >
+                  <Logs
+                    isDataFetched={isLogsFetched}
+                    data={logs}
+                  />
+                </Box>
+                <Box
+                  border={1}
+                  borderBottom={0}
+                  borderLeft={0}
+                  borderRight={0}
+                  borderColor="#e0e0e0"
+                  py={1.5}
+                  textAlign="center"
+                >
+                  <Link onClick={fetchMoreLogs}>
+                    Pobierz więcej
+                  </Link>
+                </Box>
+              </Paper>
+            )
+        }
       </Box>
       <DialogWindow
         isOpen={isDeleteComponentDialogWindowOpen}
