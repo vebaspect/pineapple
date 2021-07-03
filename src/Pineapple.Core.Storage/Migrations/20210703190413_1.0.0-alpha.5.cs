2be2300321745024643ab551d2bb58ec65dbeffd
Grzegorz Wodniczak <kontakt@vebaspect.pl>
2021-07-03 22:05:45 +0200

    API - aktualizacja migracji

---
+++ src/Pineapple.Core.Storage/Migrations/20210703190413_1.0.0-alpha.5.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Pineapple.Core.Storage.Migrations
+{
+    public partial class _100alpha5 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsImportant",
+                schema: "pineapple",
+                table: "ComponentVersions",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsImportant",
+                schema: "pineapple",
+                table: "ComponentVersions");
+        }
+    }
+}
