b343285801f55486fae9c7dcb239f4b027448eb1
Grzegorz Wodniczak <kontakt@vebaspect.pl>
2021-07-07 21:27:09 +0200

    API - aktualizacja migracji

---
+++ src/Pineapple.Core.Storage/Migrations/20210707191904_1.0.0-alpha.6_1.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Pineapple.Core.Storage.Migrations
+{
+    public partial class _100alpha6_1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "IssueTrackingSystemTicketUrl",
+                schema: "pineapple",
+                table: "ComponentVersions",
+                type: "character varying(2000)",
+                maxLength: 2000,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(300)",
+                oldMaxLength: 300,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SourceCodeRepositoryUrl",
+                schema: "pineapple",
+                table: "Components",
+                type: "character varying(2000)",
+                maxLength: 2000,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(300)",
+                oldMaxLength: 300,
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "IssueTrackingSystemTicketUrl",
+                schema: "pineapple",
+                table: "ComponentVersions",
+                type: "character varying(300)",
+                maxLength: 300,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(2000)",
+                oldMaxLength: 2000,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SourceCodeRepositoryUrl",
+                schema: "pineapple",
+                table: "Components",
+                type: "character varying(300)",
+                maxLength: 300,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(2000)",
+                oldMaxLength: 2000,
+                oldNullable: true);
+        }
+    }
+}
