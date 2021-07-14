9a993b48a2275c4cf4425e73e4cc2ede2a3f8951
Grzegorz Wodniczak <kontakt@vebaspect.pl>
2021-07-14 21:39:06 +0200

    API - aktualizacja migracji

--- src/Pineapple.Core.Storage/Migrations/20210714193835_1.0.0-alpha.10.Designer.cs
+++ src/Pineapple.Core.Storage/Migrations/20210714193835_1.0.0-alpha.10.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Pineapple.Core.Storage.Database;
@@ -9,9 +10,10 @@ using Pineapple.Core.Storage.Database;
 namespace Pineapple.Core.Storage.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20210714193835_1.0.0-alpha.10")]
+    partial class _100alpha10
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Pineapple.Core.Storage/Migrations/20210714193835_1.0.0-alpha.10.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Pineapple.Core.Storage.Migrations
+{
+    public partial class _100alpha10 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "LicensesRepositoryPath",
+                schema: "pineapple",
+                table: "Components",
+                type: "character varying(4000)",
+                maxLength: 4000,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LicensesRepositoryPath",
+                schema: "pineapple",
+                table: "Components");
+        }
+    }
+}
